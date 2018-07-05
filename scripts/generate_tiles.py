1111bac5f0cb0a8ddea55111cc38b8cef813dee6
Matthew Hartley <mhartley@cantab.net>
2018-07-05 17:50:04 +0100

    Generate all tiles

--- scripts/generate_tiles.py
+++ scripts/generate_tiles.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from collections import defaultdict
 
 import click
 
@@ -40,29 +41,64 @@ def generate_dilated_boundary_image(image_fpath):
     return 255 * dilated_boundaries.astype(np.uint8)
 
 
-def generate_tiles(dataset, output_path):
-
-
-    projection_id = 'a6b413939aea04bd96ca03b8cb7a98294c65f0fd'
-    segmentation_id = '963efa345525bce11bbb00663fcf838cb0f37c9a'
-
-    projection_fpath = dataset.item_content_abspath(projection_id)
-    segmenatation_fpath = dataset.item_content_abspath(segmentation_id)
+def generate_tiles_for_data_point(
+    projection_fpath,
+    segmentation_fpath,
+    image_output,
+    mask_output,
+    ts=256
+):
 
     projection_image = imread(projection_fpath)
+    dilated_boundaries = generate_dilated_boundary_image(segmentation_fpath)
 
-    ts = 256
+    nt = projection_fpath[-6:-4]
 
-    sx, sy = 1200, 1000
+    # One image is grayscale :/
+    try:
+        xdim, ydim, _ = projection_image.shape
+    except ValueError:
+        return
 
-    tile = projection_image[sx:sx+ts,sy:sy+ts,:]
+    nx = xdim//ts
+    ny = ydim//ts
 
-    imsave('tile.png', tile)
+    for x in range(nx):
+        for y in range(ny):
+            tile = projection_image[x*ts:(x+1)*ts,y*ts:(y+1)*ts]
+            if tile.mean() > 10:
+                imsave(image_output/'tile-{}-{}-{}.png'.format(nt, x, y), tile)
+                mask = dilated_boundaries[x*ts:(x+1)*ts,y*ts:(y+1)*ts]
+                imsave(mask_output/'tile-{}-{}-{}.png'.format(nt, x, y), mask)
 
-    dilated_boundaries = generate_dilated_boundary_image(segmenatation_fpath)
 
-    mask_tile = dilated_boundaries[sx:sx+ts,sy:sy+ts]
-    imsave('mask_tile.png', mask_tile)
+def generate_tiles(dataset, output_path):
+
+    # TODO - generate only where mask has border pixels
+
+    image_output = output_path/'image'
+    mask_output = output_path/'mask'
+    image_output.mkdir(parents=True, exist_ok=True)
+    mask_output.mkdir(parents=True, exist_ok=True)
+
+    image_types = defaultdict(dict)
+    for i in dataset.identifiers:
+        relpath = dataset.item_properties(i)['relpath']
+        datapoint_label = relpath[-7:-4]
+        image_type = relpath[:-9]
+        image_types[datapoint_label][image_type] = i
+
+    for dp_label, ids in image_types.items():
+        projection_id = ids['projection']
+        segmentation_id = ids['segmentation']
+        projection_fpath = dataset.item_content_abspath(projection_id)
+        segmentation_fpath = dataset.item_content_abspath(segmentation_id)
+        generate_tiles_for_data_point(
+            projection_fpath,
+            segmentation_fpath,
+            image_output,
+            mask_output
+        )
 
 
 @click.command()
@@ -71,7 +107,7 @@ def main(dataset_uri):
 
     dataset = DataSet.from_uri(dataset_uri)
 
-    generate_tiles(dataset, 'tile_data')
+    generate_tiles(dataset, Path('tile_data'))
 
 
 if __name__ == '__main__':
