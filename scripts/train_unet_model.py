16e3ca9eb24f3a99f2e7e98f099d259a84671d89
Matthew Hartley <mhartley@cantab.net>
2018-07-10 16:21:17 +0100

    Add script to train unet model

---
+++ scripts/train_unet_model.py
@@ -0,0 +1,292 @@
+from keras.models import Model, Sequential
+from keras.layers import Flatten, Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization
+from keras.optimizers import RMSprop
+from keras.losses import binary_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+
+import keras.backend as K
+
+from imageio import imsave
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+
+def weighted_dice_coeff(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight * weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    return score
+
+
+def weighted_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd number
+    if K.int_shape(y_pred)[1] == 128:
+        kernel_size = 11
+    elif K.int_shape(y_pred)[1] == 256:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 512:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 1024:
+        kernel_size = 41
+    else:
+        raise ValueError('Unexpected image size')
+    averaged_mask = K.pool2d(
+        y_true, pool_size=(kernel_size, kernel_size), strides=(1, 1), padding='same', pool_mode='avg')
+    border = K.cast(K.greater(averaged_mask, 0.005), 'float32') * K.cast(K.less(averaged_mask, 0.995), 'float32')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight += border * 2
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = 1 - weighted_dice_coeff(y_true, y_pred, weight)
+    return loss
+
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    # avoiding overflow
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+
+    # https://www.tensorflow.org/api_docs/python/tf/nn/weighted_cross_entropy_with_logits
+    loss = (1. - y_true) * logit_y_pred + (1. + (weight - 1.) * y_true) * \
+                                          (K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd number
+    if K.int_shape(y_pred)[1] == 128:
+        kernel_size = 11
+    elif K.int_shape(y_pred)[1] == 256:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 512:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 1024:
+        kernel_size = 41
+    else:
+        raise ValueError('Unexpected image size')
+    averaged_mask = K.pool2d(
+        y_true, pool_size=(kernel_size, kernel_size), strides=(1, 1), padding='same', pool_mode='avg')
+    border = K.cast(K.greater(averaged_mask, 0.005), 'float32') * K.cast(K.less(averaged_mask, 0.995), 'float32')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight += border * 2
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + (1 - weighted_dice_coeff(y_true, y_pred, weight))
+    return loss
+
+
+def get_unet_256(input_shape=(256, 256, 3),
+                 num_classes=1):
+
+    inputs = Input(shape=input_shape)
+    # 256
+
+    down0 = Conv2D(32, (3, 3), padding='same')(inputs)
+    down0 = BatchNormalization()(down0)
+    down0 = Activation('relu')(down0)
+    down0 = Conv2D(32, (3, 3), padding='same')(down0)
+    down0 = BatchNormalization()(down0)
+    down0 = Activation('relu')(down0)
+    down0_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0)
+    # 128
+
+    down1 = Conv2D(64, (3, 3), padding='same')(down0_pool)
+    down1 = BatchNormalization()(down1)
+    down1 = Activation('relu')(down1)
+    down1 = Conv2D(64, (3, 3), padding='same')(down1)
+    down1 = BatchNormalization()(down1)
+    down1 = Activation('relu')(down1)
+    down1_pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+    # 64
+
+    down2 = Conv2D(128, (3, 3), padding='same')(down1_pool)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation('relu')(down2)
+    down2 = Conv2D(128, (3, 3), padding='same')(down2)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation('relu')(down2)
+    down2_pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+    # 32
+
+    down3 = Conv2D(256, (3, 3), padding='same')(down2_pool)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation('relu')(down3)
+    down3 = Conv2D(256, (3, 3), padding='same')(down3)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation('relu')(down3)
+    down3_pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+    # 16
+
+    down4 = Conv2D(512, (3, 3), padding='same')(down3_pool)
+    down4 = BatchNormalization()(down4)
+    down4 = Activation('relu')(down4)
+    down4 = Conv2D(512, (3, 3), padding='same')(down4)
+    down4 = BatchNormalization()(down4)
+    down4 = Activation('relu')(down4)
+    down4_pool = MaxPooling2D((2, 2), strides=(2, 2))(down4)
+    # 8
+
+    center = Conv2D(1024, (3, 3), padding='same')(down4_pool)
+    center = BatchNormalization()(center)
+    center = Activation('relu')(center)
+    center = Conv2D(1024, (3, 3), padding='same')(center)
+    center = BatchNormalization()(center)
+    center = Activation('relu')(center)
+    # center
+
+    up4 = UpSampling2D((2, 2))(center)
+    # up4 = concatenate([down4, up4], axis=3)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    # 16
+
+    up3 = UpSampling2D((2, 2))(up4)
+    # up3 = concatenate([down3, up3], axis=3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    # 32
+
+    up2 = UpSampling2D((2, 2))(up3)
+    # up2 = concatenate([down2, up2], axis=3)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    # 64
+
+    up1 = UpSampling2D((2, 2))(up2)
+    # up1 = concatenate([down1, up1], axis=3)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    # 128
+
+    up0 = UpSampling2D((2, 2))(up1)
+    # up0 = concatenate([down0, up0], axis=3)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    # 256
+
+    classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(up0)
+
+    model = Model(inputs=inputs, outputs=classify)
+
+    model.compile(optimizer=RMSprop(lr=0.0001), loss=bce_dice_loss, metrics=[dice_coeff])
+
+    return model
+
+
+def train_model():
+
+    data_gen_args = dict( # featurewise_center=True,
+                     # featurewise_std_normalization=True,
+                     rotation_range=90.,
+                     width_shift_range=0.1,
+                     height_shift_range=0.1,
+                     zoom_range=0.2)
+
+    image_datagen = ImageDataGenerator(**data_gen_args)
+    mask_datagen = ImageDataGenerator(**data_gen_args)
+
+    seed = 1
+
+    # image_datagen.fit(images, augment=True, seed=seed)
+    # mask_datagen.git(masks, augment=True, seed=seed)
+
+    image_generator = image_datagen.flow_from_directory(
+        'tile_data/image',
+        class_mode=None,
+        seed=seed,
+        batch_size=32)
+    mask_generator = mask_datagen.flow_from_directory(
+        'tile_data/mask',
+        color_mode='grayscale',
+        class_mode=None,
+        seed=seed,
+        batch_size=32)
+
+
+    # result = image_generator.next()
+    # mask_result = mask_generator.next()
+
+    # print(mask_result.shape)
+    # imsave('sample.png', result[1])
+    # imsave('mask_sample.png', mask_result[1])
+
+    train_generator = zip(image_generator, mask_generator)
+
+    model = get_unet_256()
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=10,
+        verbose=1
+    )
+
+    model.save('unet256.h5')
+
+def main():
+
+    # model = get_unet_256()
+    # model.summary()
+    train_model()
+
+
+if __name__ == '__main__':
+    main()
